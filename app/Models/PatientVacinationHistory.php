4df886d644083e151bdc97eb4cc7a0c1febe4c8e
acecavaliers <95069460+acecavaliers@users.noreply.github.com>
2024-05-17 16:49:45 +0800

    FORM

--- app/Models/PatientVacinationHistory.php
+++ app/Models/PatientVacinationHistory.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PatientVacinationHistory extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 }
